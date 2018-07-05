52c1443a819d23d63e73ffb47025c37778ac9b05
Pablo Rivera <priveras@gmail.com>
2018-07-05 15:34:32 -0400

    update

--- atomic/settings.py
+++ atomic/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
