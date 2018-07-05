9e30cb427b89470dc564e94cb6eb266fc64728c2
Pablo Rivera <priveras@gmail.com>
2018-07-05 15:23:59 -0400

    update

--- atomic/urls.py
+++ atomic/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('blog.urls')),
     url(r'^', include('dashboard.urls')),
+    url(r'^', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
