a5d4b406c3e2bdf938b07dc781afd38ff73382bb
Pablo Rivera <priveras@gmail.com>
2018-07-05 15:20:56 -0400

    update

--- atomic/settings.py
+++ atomic/settings.py
@@ -36,9 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
